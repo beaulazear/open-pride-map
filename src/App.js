da197314f35d96869ce108571652a2ef5f5d0eb3
Beau Lazear <beaulazear@gmail.com>
2025-02-22 15:08:24 -0500

    chang logo to pride png. Change background gradient to be pride flag colors. added styling for header. added footer component and styled like header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 import Map from './components/Map';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="header-content">
+          <p>Open Pride Maps</p>
+          <a href="https://www.vecteezy.com/free-png/pride" className="credit-link">
+            Pride PNGs by Vecteezy
+          </a>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Open Pride Maps
-        </p>
       </header>
       <Map />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="App-footer">
+            <a href="https://www.vecteezy.com/free-png/pride" className="credit-link">
+                Pride PNGs by Vecteezy
+            </a>
+        </footer>
+    );
+}
