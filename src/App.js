7ff721e01a4c6584fcdb19a111ec7d8179412d89
Beau Lazear <beaulazear@gmail.com>
2025-02-22 15:17:40 -0500

    update styling for header and changed locations of logo and title

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <div className="header-content">
           <p>Open Pride Maps</p>
           <a href="https://www.vecteezy.com/free-png/pride" className="credit-link">
             Pride PNGs by Vecteezy
           </a>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
       </header>
       <Map />
       <Footer />
